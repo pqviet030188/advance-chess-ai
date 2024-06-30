e44cf25465448ab766c3cb5c2a590fb50c21ef4f
vietphung <viet.phung@truii.com>
2024-06-30 18:45:05 +1000

    dragon miner general check

---
+++ tests/pgeneral/general_being_attacked_miner_test.go
@@ -0,0 +1,447 @@
+package pgeneral_test
+
+import (
+	"testing"
+
+	"github.com/pqviet030188/advance-chess-ai/bitboard"
+	"github.com/pqviet030188/advance-chess-ai/gamemodel"
+)
+
+func SetupWithMiner(wall string, nearGeneral string, farGeneral string, farMiner string) *gamemodel.GameModel {
+	side := bitboard.NEAR
+
+	model := gamemodel.SetupWithFacts(side)
+	model.NearGeneral = bitboard.NewBitboardFromStr(nearGeneral)
+	model.FarGeneral = bitboard.NewBitboardFromStr(farGeneral)
+	model.FarMiner = bitboard.NewBitboardFromStr(farMiner)
+	model.Wall = bitboard.NewBitboardFromStr(wall)
+
+	gamemodel.Update(model, side)
+	return model
+}
+
+func TestGeneralBeingAttackedByMiner1(t *testing.T) {
+	model := SetupWithMiner(`
+		000000000
+		000000000
+		000000000
+		000000000
+		000000000
+		000000000
+		000000000
+		000000000
+		000000000
+	`, `
+		000000000
+		000000000
+		000000000
+		000000000
+		000000000
+		000000000
+		000000000
+		000000000
+		000010000
+	`, `
+		000010000
+		000000000
+		000000000
+		000000000
+		000000000
+		000000000
+		000000000
+		000000000
+		000000000
+	`, `
+		000000000
+		000000000
+		000000000
+		000000000
+		000000000
+		000000000
+		000010000
+		000000000
+		000000000
+	`)
+
+	attacked := model.IsGeneralBeingAttacked(bitboard.E1, bitboard.NEAR)
+
+	if !attacked {
+		t.Errorf("Expected to be threatened")
+	}
+}
+
+func TestGeneralBeingAttackedByMiner2(t *testing.T) {
+	model := SetupWithMiner(`
+		000000000
+		000000000
+		000000000
+		000000000
+		000000000
+		000000000
+		000000000
+		000000000
+		000000000
+	`, `
+		000000000
+		000000000
+		000000000
+		000000000
+		000000000
+		000000000
+		000000000
+		000000000
+		000010000
+	`, `
+		000010000
+		000000000
+		000000000
+		000000000
+		000000000
+		000000000
+		000000000
+		000000000
+		000000000
+	`, `
+		000000000
+		000000000
+		000000000
+		000000000
+		000000000
+		000000000
+		000000000
+		000000000
+		000000010
+	`)
+
+	attacked := model.IsGeneralBeingAttacked(bitboard.E1, bitboard.NEAR)
+
+	if !attacked {
+		t.Errorf("Expected to be threatened")
+	}
+}
+
+func TestGeneralBeingAttackedByMiner3(t *testing.T) {
+	model := SetupWithMiner(`
+		000000000
+		000000000
+		000000000
+		000000000
+		000000000
+		000000000
+		000000000
+		000000000
+		000000000
+	`, `
+		000000000
+		000000000
+		000000000
+		000000000
+		000000000
+		000000000
+		000000000
+		000000000
+		000010000
+	`, `
+		000010000
+		000000000
+		000000000
+		000000000
+		000000000
+		000000000
+		000000000
+		000000000
+		000000000
+	`, `
+		000000000
+		000000000
+		000000000
+		000000000
+		000000000
+		000000000
+		001000000
+		000000000
+		000000000
+	`)
+
+	attacked := model.IsGeneralBeingAttacked(bitboard.E1, bitboard.NEAR)
+
+	if attacked {
+		t.Errorf("Expected not to be threatened")
+	}
+}
+
+func TestGeneralBeingAttackedByMiner4(t *testing.T) {
+	model := SetupWithMiner(`
+		000000000
+		000000000
+		000000000
+		000000000
+		000000000
+		000000000
+		000000000
+		000000000
+		000000000
+	`, `
+		000000000
+		000000000
+		000000000
+		000000000
+		000000000
+		000000000
+		000000000
+		000000000
+		000010000
+	`, `
+		000010000
+		000000000
+		000000000
+		000000000
+		000000000
+		000000000
+		000000000
+		000000000
+		000000000
+	`, `
+		000000000
+		000000000
+		000000000
+		000000000
+		000000000
+		000000000
+		000000100
+		000000000
+		000000000
+	`)
+
+	attacked := model.IsGeneralBeingAttacked(bitboard.E1, bitboard.NEAR)
+
+	if attacked {
+		t.Errorf("Expected not to be threatened")
+	}
+}
+
+func TestGeneralBeingAttackedByMinerWithWallBlockers(t *testing.T) {
+	model := SetupWithMiner(`
+		000000000
+		000000000
+		000000000
+		000000000
+		000000000
+		000000000
+		000000000
+		000111000
+		000000000
+	`, `
+		000000000
+		000000000
+		000000000
+		000000000
+		000000000
+		000000000
+		000000000
+		000000000
+		000010000
+	`, `
+		000010000
+		000000000
+		000000000
+		000000000
+		000000000
+		000000000
+		000000000
+		000000000
+		000000000
+	`, `
+		000000000
+		000000000
+		000000000
+		000000000
+		000000000
+		000000000
+		000010000
+		000000000
+		000000000
+	`)
+
+	attacked := model.IsGeneralBeingAttacked(bitboard.E1, bitboard.NEAR)
+
+	if attacked {
+		t.Errorf("Expected not to be threatened")
+	}
+}
+
+func TestGeneralBeingAttackedByMinerWithAllyBlockers(t *testing.T) {
+	model := SetupWithMiner(`
+		000000000
+		000000000
+		000000000
+		000000000
+		000000000
+		000000000
+		000000000
+		000000000
+		000000000
+	`, `
+		000000000
+		000000000
+		000000000
+		000000000
+		000000000
+		000000000
+		000000000
+		000000000
+		000010000
+	`, `
+		000010000
+		000000000
+		000000000
+		000000000
+		000000000
+		000000000
+		000000000
+		000000000
+		000000000
+	`, `
+		000000000
+		000000000
+		000000000
+		000000000
+		000000000
+		000000000
+		000010000
+		000000000
+		000000000
+	`)
+
+	model.NearBuilder = bitboard.NewBitboardFromStr(`
+		000000000
+		000000000
+		000000000
+		000000000
+		000000000
+		000000000
+		000000000
+		000111000
+		000000000
+	`)
+
+	gamemodel.Update(model, bitboard.NEAR)
+	attacked := model.IsGeneralBeingAttacked(bitboard.E1, bitboard.NEAR)
+
+	if attacked {
+		t.Errorf("Expected not to be threatened")
+	}
+}
+
+func TestGeneralBeingAttackedByMinerWithProtection(t *testing.T) {
+	model := SetupWithMiner(`
+		000000000
+		000000000
+		000000000
+		000000000
+		000000000
+		000000000
+		000000000
+		000000000
+		000000000
+	`, `
+		000000000
+		000000000
+		000000000
+		000000000
+		000000000
+		000000000
+		000000000
+		000000000
+		000010000
+	`, `
+		000010000
+		000000000
+		000000000
+		000000000
+		000000000
+		000000000
+		000000000
+		000000000
+		000000000
+	`, `
+		000000000
+		000000000
+		000000000
+		000000000
+		000000000
+		000000000
+		000010000
+		000000000
+		000000000
+	`)
+
+	model.NearSentinel = bitboard.NewBitboardFromStr(`
+		000000000
+		000000000
+		000000000
+		000000000
+		000000000
+		000000000
+		000000000
+		000000000
+		000001000
+	`)
+
+	gamemodel.Update(model, bitboard.NEAR)
+	attacked := model.IsGeneralBeingAttacked(bitboard.E1, bitboard.NEAR)
+
+	if attacked {
+		t.Errorf("Expected not to be threatened")
+	}
+}
+
+func TestGeneralBeingAttackedByMinerInNearby(t *testing.T) {
+	model := SetupWithMiner(`
+		000000000
+		000000000
+		000000000
+		000000000
+		000000000
+		000000000
+		000000000
+		000000000
+		000000000
+	`, `
+		000000000
+		000000000
+		000000000
+		000000000
+		000000000
+		000000000
+		000000000
+		000000000
+		000010000
+	`, `
+		000010000
+		000000000
+		000000000
+		000000000
+		000000000
+		000000000
+		000000000
+		000000000
+		000000000
+	`, `
+		000000000
+		000000000
+		000000000
+		000000000
+		000000000
+		000000000
+		000000000
+		000000000
+		000100000
+	`)
+
+	attacked := model.IsGeneralBeingAttacked(bitboard.E1, bitboard.NEAR)
+
+	if !attacked {
+		t.Errorf("Expected to be threatened")
+	}
+}
